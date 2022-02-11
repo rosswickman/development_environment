c24893f80bc398559cd6580f123b40be86402b96
Ross Wickman <ross@rosswickman.com>
2022-02-11 16:12:55 -0600

    add backup with date time

--- scripts/curl-aliases.sh
+++ scripts/curl-aliases.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+now=$(date +%F_%H-%M-%S)
+
+echo "Making backup of existing files if they exist"
+cp ~/.bash_profile ~/.bash_proflie.bak-$now
+cp ~/.bashrc ~/.bashrc.bak-$now
+cp ~/.zprofile ~/.zproflie.bak-$now
+cp ~/.zshrc ~/.zshrc.bak-$now
+
 echo "Download Aliases File"
 
 curl https://raw.githubusercontent.com/rosswickman/development_environment/master/files/aliases > ~/.bash_profile
